4b8ef513cd8c4cba09bdaf3dc33109f5d69b30a3
Dylan Murayama <dmurayama93@gmail.com>
2024-01-13 14:17:19 -1000

    add clear function

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// operators: + - * /
+
+switch(operator){
+    case '+':
+        return;
+        break;
+    case '-':
+        return;
+    case '*':
+        return;
+    case '/':
+        return;
+}
+
+// can manipulate total until clear is hit
+let total = 0;
+
+// clear function: reset total back to 0
+function clear(){
+    return total = 0;
+}
