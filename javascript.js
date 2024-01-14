39468d41d1931f50bfefe60550fb1a1d8adaadc2
Dylan Murayama <dmurayama93@gmail.com>
2024-01-13 15:51:11 -1000

    add number btns and functions to handle number input

--- javascript.js
+++ javascript.js
@@ -1,14 +1,28 @@
+let total = 0;
+let newNumber;
+let numberArray = [];
+
 function setOperator(operator) {
     switch(operator){
         case '+':
+            // after operator selected, input newnumber
+            // this has to be executed after number input and equal sign hit
+            total += newNumber;
             break;
         case '-':
+            total -= newNumber;
             break;
         case '*':
+            total *= newNumber;
             break;
         case '/':
+            total /= newNumber;
             break;
     }
+}
+
+function getNewNumber() {
+    prompt()
 }
 
 // operator buttons
@@ -34,10 +48,36 @@ divideBtn = addEventListener("click", () => {
     setOperator("/");
 })
 
-// can manipulate total until clear is hit
-let total = 0;
+// number Buttons
+const zeroBtn = document.querySelector(".zeroBtn");
+const oneBtn = document.querySelector(".oneBtn");
+const twoBtn = document.querySelector(".twoBtn");
+const threeBtn = document.querySelector(".threeBtn");
+const fourBtn = document.querySelector(".fourBtn");
+const fiveBtn = document.querySelector(".fiveBtn");
+const sixBtn = document.querySelector(".sixBtn");
+const sevenBtn = document.querySelector(".sevenBtn");
+const eightBtn = document.querySelector(".eightBtn");
+const nineBtn = document.querySelector(".nineBtn");
+
+// number btn event listener
+zeroBtn = addEventListener("click", () => {
+
+})
 
 // clear function: reset total back to 0
 function clear(){
     return total = 0;
+}
+
+// joins array of numbers and then converts back to number
+// set number when operator symbol is clicked
+function setNumber(){
+    newNumber = +numberArray.join('');
+    clearNumberArray();
+}
+
+// clear number array when number is set
+function clearNumberArray() {
+    numberArray = [];
 }
