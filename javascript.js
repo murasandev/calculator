837e28069a72027bc7caa7237048eaa5dad213cf
Dylan Murayama <dmurayama93@gmail.com>
2024-01-15 17:50:13 -1000

    add basic calculation function to display on screen

--- javascript.js
+++ javascript.js
@@ -1,23 +1,28 @@
 let total = 0;
 let newNumber;
-let numberArray = [];
+let digitArray = [];
+let numArray = [];
 let operator;
 
-function evalOperation(operator) {
-    switch(operator){
+let equation = {
+    total: '',
+    number: '',
+    operator: ''
+}
+
+function evalEquation() {
+    switch(equation.operator){
         case '+':
-            // after operator selected, input newnumber
-            // this has to be executed after number input and equal sign hit
-            total += newNumber;
+            total = +equation.total + +equation.number;
             break;
         case '-':
-            total -= newNumber;
+            total = +equation.total - +equation.number;
             break;
         case '*':
-            total *= newNumber;
+            total = +equation.total * +equation.number;
             break;
         case '/':
-            total /= newNumber;
+            total = +equation.total / +equation.number;
             break;
     }
 }
@@ -26,16 +31,27 @@ const screenBot = document.querySelector(".screen-bot");
 const screenTop = document.querySelector(".screen-top");
 
 const screenBotText = document.createElement("div");
+const screenTopText = document.createElement("div");
 
-function setTextScreenBot(string) {
-    string = numberArray.join("");
+function setTextScreenBot() {
+    string = digitArray.join("");
     screenBotText.innerText = `${string}`;
     screenBot.appendChild(screenBotText);
 }
 
+function setTextScreenTop() {
+    string = numArray.join(" ");
+    screenTopText.innerText = `${string}`;
+    screenTop.appendChild(screenTopText);
+}
+
 function resetScreen() {
-    screenBotText.innerText = `${0}`;
+    digitArray = [];
+    numArray = [];
+    screenBotText.innerText = 0;
+    screenTopText.innerText = '';
     screenBot.appendChild(screenBotText);
+    screenTop.appendChild(screenTopText);
 }
 
 resetScreen();
@@ -47,30 +63,56 @@ const multiplyBtn = document.querySelector(".multiplyBtn");
 const divideBtn = document.querySelector(".divideBtn");
 const equalBtn = document.querySelector(".equalBtn");
 const clearBtn = document.querySelector(".clearBtn");
+
 // button eventlisteners
 addBtn.addEventListener("click", () => {
-    operator = '+';
+    operator = '+';   
+    // if(typeof numArray[numArray.length - 1] === 'number' || equation.operator === ''){
+    //     setNumber();   
+    //     setOperator();
+    // }      
+    // else setOperator();
+    setNumber();   
+    setOperator();
 });
 
 subtractBtn.addEventListener("click", () => {
     operator = '-';
+    if(equation.operator === ''){
+        setNumber();   
+        setOperator();
+    }      
+    else setOperator();
 });
 
 multiplyBtn.addEventListener("click", () => {
     operator = '*';
+    if(equation.operator === ''){
+        setNumber();   
+        setOperator();
+    }      
+    else setOperator();
 });
 
 divideBtn.addEventListener("click", () => {
     operator = '/';
+    if(equation.operator === ''){
+        setNumber();   
+        setOperator();
+    }      
+    else setOperator();
 });
 
 equalBtn.addEventListener("click", () => {
     operator = '=';
-    clearNumber();
+    setNumber();
+    evalEquation();
+    setTotal();
+    console.log(total);
 });
 
 clearBtn.addEventListener("click", () => {
-    clearNumber();
+    resetScreen();
 })
 
 // number Buttons
@@ -88,78 +130,115 @@ const pointBtn = document.querySelector(".pointBtn");
 
 // number btn event listener
 zeroBtn.addEventListener("click", () => {
-    numberArray.push("0");
+    digitArray.push("0");
     setTextScreenBot();
 })
 
 oneBtn.addEventListener("click", () => {
-    numberArray.push("1");
+    digitArray.push("1");
     setTextScreenBot();
 })
 
 twoBtn.addEventListener("click", () => {
-    numberArray.push("2");
+    digitArray.push("2");
     setTextScreenBot();
 })
 
 threeBtn.addEventListener("click", () => {
-    numberArray.push("3");
+    digitArray.push("3");
     setTextScreenBot();
 })
 
 fourBtn.addEventListener("click", () => {
-    numberArray.push("4");
+    digitArray.push("4");
     setTextScreenBot();
 })
 
 fiveBtn.addEventListener("click", () => {
-    numberArray.push("5");
+    digitArray.push("5");
     setTextScreenBot();
 })
 
 sixBtn.addEventListener("click", () => {
-    numberArray.push("6");
+    digitArray.push("6");
     setTextScreenBot();
 })
 
 sevenBtn.addEventListener("click", () => {
-    numberArray.push("7");
+    digitArray.push("7");
     setTextScreenBot();
 })
 
 eightBtn.addEventListener("click", () => {
-    numberArray.push("8");
+    digitArray.push("8");
     setTextScreenBot();
 })
 
 nineBtn.addEventListener("click", () => {
-    numberArray.push("9");
+    digitArray.push("9");
     setTextScreenBot();
 })
 
 pointBtn.addEventListener("click", () => {
-    numberArray.push(".");
+    digitArray.push(".");
     setTextScreenBot();
 })
 
-// clear function: reset total back to 0
-function clearTotal(){
-    return total = 0;
-}
-
-// joins array of numbers and then converts back to number
-// set number when operator symbol is clicked
-function setNumber(){
-    newNumber = +numberArray.join('');
-    clearNumber();
-}
-
 // clear number array when number is set
 function clearNumber() {
-    numberArray = [];
+    digitArray = [];
     newNumber = 0;
     setTextScreenBot();
     resetScreen(0);
 }
 
-// set number > set operator > set number > evaluate
+// set two arrays one for numbers and one for operator
+// whenever an operator function or equal is clicked
+// print through both arrays
+// ex. 
+// arrayNumber = [1, 4, 6, 7]
+// arrayOperator = [+, -, *, =]
+// return 1 + 4 - 6 * 7 =
+
+// if numArr.length === opArr.length 
+// opArr[length - 1] = current operator
+
+// use when operator symbols are used
+function setNumber() {
+    newNumber = digitArray.join("");
+    if(equation.total === ''){
+        equation.total = newNumber;
+        console.log(`equation num1 is ${equation.total}`);
+    }
+    else {
+        equation.number = newNumber;
+        console.log(`equation num2 is ${equation.number}`);
+    }
+    numArray.push(newNumber);
+
+    console.log(numArray);
+    digitArray = [];
+    // may need to move bottom line
+    setTextScreenTop();
+}
+
+function setOperator(){
+    if(equation.operator === ''){
+        equation.operator = operator;
+        console.log(`equation operator is ${equation.operator}`);
+        numArray.push(operator);
+        setTextScreenTop();
+    }
+    // else {
+    //     equation.operator = operator;
+    //     console.log(`equation operator is ${equation.operator}`);
+    //     delete numArray[numArray.length - 1];
+    //     numArray.push(operator);
+    //     setTextScreenTop();
+    // }
+}
+
+function setTotal(){
+    screenBotText.innerText = `${total}`;
+    screenBot.appendChild(screenBotText);
+}
